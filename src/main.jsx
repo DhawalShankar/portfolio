763ebf89f7ab1aec3e7c83a7450804eb96797d24
DhawalShankar <dhawalmannu@gmail.com>
2025-12-03 20:40:15 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import HackerPortfolio from './Writer.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
-  {
-    path: "/writer",
-    element: <HackerPortfolio />,
-  },
+ 
   // Add more routes here as needed
   // {
   //   path: "/about",
